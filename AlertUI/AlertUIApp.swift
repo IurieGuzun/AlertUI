0f2b3292f683522bd19320b3e434463d21a73479
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-25 10:53:00 -0500

    Initial Commit

---
+++ AlertUI/AlertUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertUIApp.swift
+//  AlertUI
+//
+//  Created by Iurie Guzun on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct AlertUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
